09f10e823af21e915a75ef3db6d8e03d70c3e7f0
Matteo Giachino <matteo.giachino@prima.it>
2023-11-23 14:02:49 +0100

    expose Error type

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,9 @@ mod error;
 mod matcher;
 mod parser;
 
+pub use error::Error;
 pub use matcher::scim_filter;
 use serde::Serialize;
-
-use crate::error::Error;
 
 /// Import this trait to add to every type `x` that can be made into an iterator over I: Serialize
 /// a `x.scim_filter(input: &str)` function that return a result with a vector of filtered I
