8e63f91ad52f8ce65b81d2469b67cf39448b9d2c
Matteo Giachino <matteo.giachino@prima.it>
2023-11-23 13:36:44 +0100

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use serde::Serialize;
 
 use crate::error::Error;
 
+/// Import this trait to add to every type `x` that can be made into an iterator over I: Serialize
+/// a `x.scim_filter(input: &str)` function that return a result with a vector of filtered I
 pub trait ScimFilter {
     type Item: Serialize;
     fn scim_filter(self, input: &str) -> Result<Vec<Self::Item>, Error>;
